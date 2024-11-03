2a04acb25dda633a36caa0703edd8a8a85ca0693
jiyoung <jypark912@gmail.com>
2024-11-03 22:40:38 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+      <h1>영화제목</h1>
+      <div v-for="(movie, i) in data" :key="i" class="item">
+        <figure>
+          <img :src="`${movie.imgUrl}`" :alt="movie.title">
+        </figure>
+        <div class="info">
+          <h3 class="text" :style="movie.style">{{ movie.title }}</h3>
+          <p> 개봉: {{ movie.year }}</p>
+          <p>장르: {{ movie.category }}</p>
+          <button @:click="increseLike(i)">좋아요</button>
+          <span>{{ movie.like }}</span>
+          <p>
+            <button @click="isModal=true; selectedMovie=i">상세보기</button>
+          </p>
+        </div>
+      </div>
+      <div class="modal" v-if="isModal">
+          <div class="inner">
+            <h3>{{ data[selectedMovie].title }}</h3>
+            <p>영화 상세정보</p>
+            <button @click="isModal=false">닫기</button>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import data from './assets/movies';
+
+  export default {
+    name : 'App',
+    data() {
+      return {
+        isModal: false,
+        data: data,
+        selectedMovie: 0,
+      }
+    },
+    methods: {
+      increseLike(i) {
+        this.data[i].like += 1;
+      }
+    }
+  }
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  body {
+    max-width: 768px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+
+  h1, h2, h3 {
+    margin-bottom: 1rem;
+  }
+
+  p {
+    margin-bottom: 0.5rem;
+  }
+
+  button {
+    margin-right: 10px;
+    margin-top: 1rem;
+  }
+
+  .item {
+    width: 100%;
+    border: 1px solid #ccc;
+    display: flex;
+    margin-bottom: 20px;
+    padding: 1rem;
+  }
+
+  .item figure {
+    width: 30%;
+    margin-right: 1rem;
+  }
+
+  .item img {
+    width: 100%;
+  }
+
+  .item .info {
+    width: 100%;
+  }
+
+  .modal {
+    background: rgba(0,0,0,0.7);
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal .inner {
+    background: #fff;
+    width: 80%;
+    padding: 20px;
+    border-radius: 10px;
+  }
+</style>
---
+++ src/assets/movies.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        title: '어바웃타임',
+        year: 2013,
+        category: '로맨틱, 코미디',
+        like: 0,
+        imgUrl: './assets/about_time.webp'
+    },
+    {
+        title: '인턴', 
+        year: 2015,
+        category: '코미디',
+        like: 0,
+        imgUrl: './assets/intern.webp'
+    },
+    {
+        title: '이웃집 토토로',
+        year: 1988,
+        category: '가족영화',
+        like: 0,
+        imgUrl: './assets/totoro.webp'
+    },
+]
+
+
+export default data;
